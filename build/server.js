ad2fdb05129d660a78a34be31b20cc03357c4f99
マム【mamu】 <84264130+shirakomamu@users.noreply.github.com>
2022-09-30 15:30:24 -0700

    Randomize nextID to be in range 1000~2000 (#16)

--- build/server.js
+++ build/server.js
@@ -12,7 +12,7 @@ async function start() {
     const server = fastify();
     // starting at an arbitrary number instead of 1 to avoid candidates conflating
     // this ID with an index in to the list of numbers they are asked to dial.
-    let nextID = 1234;
+    let nextID = 1000 + Math.round(Math.random() * 1000);
     server.get("/", async () => {
         return `The API server only supports POST requests to ${CALL_PATH}\n`;
     });
